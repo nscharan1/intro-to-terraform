c2c15737687351be537b67498a19af1e1805ad07
nscharan1 <50562264+nscharan1@users.noreply.github.com>
2020-05-16 01:44:12 -0400

    Update variables.tf

--- single-web-server/variables.tf
+++ single-web-server/variables.tf
@@ -10,8 +10,17 @@
 # OPTIONAL PARAMETERS
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
+variable "ami_id" {
+  description = "CENTOS AMI ID"
+  default     = "ami-0affd4508a5d2481b"
 }
+
+
+variable "instance_id" {
+  description = "INSTANCE TYPE"
+  default     = "t2.micro"
+}
+
+variable "key_id" {
+  description = "keyname"
+  default     = "pri"
